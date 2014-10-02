2367b74c455c965b9e5d30943cf2367b0d120a23
Timo Teräs <timo.teras@iki.fi>
2014-10-02 09:07:20 +0300

    lua/example: set test picture on camera power off

--- lua/example.lua
+++ lua/example.lua
@@ -8,6 +8,13 @@ local AtemMixer = require 'AtemMixer'
 local PanasonicAW = require 'PanasonicAW'
 local lastpowerclick = 0
 
+local c = Control:new()
+
+local function powerOnOff(onoff)
+	if onoff == false then c.mixer:setProgram(1000) end
+	c:powerOnOff(onoff)
+end
+
 local function durToSpeed(dur, st)
 	if not st then return 0 end
 	if dur < 1.5 then return 10 end
@@ -15,7 +22,6 @@ local function durToSpeed(dur, st)
 	return 30
 end
 
-local c = Control:new()
 c:addInput("/dev/input/by-id/usb-G-Tech_Wireless_Dongle-event-kbd")
 c:addInput("/dev/input/by-id/usb-G-Tech_Wireless_Dongle-event-mouse")
 c:setMixer(AtemMixer:new("192.168.0.10"))
@@ -42,6 +48,6 @@ c:bindKeyDn(k.KEY_8, function() c.cameras[6]:gotoPreset(5) end)
 c:bindKeyDn(k.KEY_9, function() c.cameras[6]:gotoPreset(6) end)
 c:bindKeyDn(k.KEY_ENTER, function() c.mixer:doCut() end)
 c:bindKeyDn(k.KEY_PROPS, function() c.mixer:autoDownstreamKey(0) end)
-c:bindKeyDn(k.KEY_SLEEP, function() local now = os.time() c:powerOnOff(now-lastpowerclick >= 2.0) lastpowerclick=now end)
+c:bindKeyDn(k.KEY_SLEEP, function() local now = os.time() powerOnOff(now-lastpowerclick >= 2.0) lastpowerclick=now end)
 
 c:run()
